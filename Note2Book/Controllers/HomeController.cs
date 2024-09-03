f9ca8748a78c498f18e97f7cd7b19a2a2d3aec68
Odinson137 <yuriy_b@holdingbp.ru>
2024-09-03 21:43:22 +0300

    fix

---
+++ Note2Book/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Note2Book.Models;
+
+namespace Note2Book.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var model = new
+        {
+            Name = "sdf",
+            Test = "sdf"
+        };
+        return View(model);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Note2Book/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Note2Book.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
